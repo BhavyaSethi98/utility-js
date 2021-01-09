9a1b3a2937362a5152932b3bb6b26bf4a2721f6d
bhavya <sethibhavya1998@gmail.com>
2021-01-10 02:06:17 +0530

    Handled undefined value

--- src/reduce.js
+++ src/reduce.js
@@ -1,13 +1,17 @@
 const reduce = (input, reducer, additionalValue) =>{
 	let result;
 	if (additionalValue == undefined) {
-		result=undefined
+	   if(typeof(input)=="string")
+	     result="";
+	   else if (typeof(input)=="number")
+	     result=0;
+	   else
+		 result=undefined
 	}
 	else{ result = additionalValue }
 
 	for ( let i = 0; i < input.length; i++){
 		result = reducer(result, input[i], i, input.values());
-		console.log(result);
 	}
 	return result;
 }
